cee66e848f106e872d08d2bd688482d780178792
mangoot9669 <mangoot9669@gmail.com>
2025-05-05 01:35:10 +0300

    Create index.js

---
+++ pp/styles/index.js
@@ -0,0 +1,11 @@
+export const themes = {
+  dark: {
+    backgroundColor: '#2E2E2E',
+    chatBackground: 'url(./assets/camouflage.png)',
+    textColor: '#D3D3D3',
+    buttonBackground: '#4B5320',
+    borderColor: '#1A1A1A',
+    fontFamily: 'Courier New',
+  },
+};
+export const defaultTheme = 'dark';
