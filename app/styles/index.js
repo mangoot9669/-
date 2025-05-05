25000a080d857478980ba0b6bec9454415a250b0
mangoot9669 <mangoot9669@gmail.com>
2025-05-05 08:39:01 +0300

    Create app/styles/index.js for Protector 101

---
+++ app/styles/index.js
@@ -0,0 +1,12 @@
+
+export const themes = {
+  dark: {
+    backgroundColor: '#2E2E2E', // Dark military tone
+    chatBackground: 'url(./assets/splash_background.png)',
+    textColor: '#D3D3D3',
+    buttonBackground: '#4B5320', // Army green
+    borderColor: '#1A1A1A',
+    fontFamily: 'Courier New', // Tactical monospace font
+  },
+};
+export const defaultTheme = 'dark';
